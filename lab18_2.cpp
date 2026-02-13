a5097eb93695eaf967d000c5575adebd886182e3
Nipasiri <sayfanacha@gmail.com>
2026-02-13 19:41:58 +0700

    lab18_2

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect A, Rect B){
+    double x_overlap = min(A.x + A.w, B.x + B.w) - max(A.x, B.x);
+    double y_overlap = min(A.y, B.y) - max(A.y-A.h, B.y-B.h);
+    if(x_overlap > 0 && y_overlap > 0){
+        return x_overlap * y_overlap;
+    }
+    else{
+        return 0;
+    }
 }
